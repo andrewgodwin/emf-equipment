759c2f87c1076c3d55e132424673f4465ef0d289
Andrew Godwin <andrew@aeracode.org>
2024-03-29 12:41:06 -0600

    Some styling, class-based views

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
 import os
 import time
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django import forms
+from django.views.generic.edit import FormView
 from django.singlefile import SingleFileApp
 from grist_api import GristDocAPI
 
@@ -24,46 +25,45 @@ def index(request):
     return render(request, "index.html")
 
 
-class CheckoutForm(forms.Form):
-    location = forms.CharField(max_length=200)
-
-
 @app.path("<int:tool_id>/")
-def tool(request, tool_id):
-    """
-    Individual tool checkout page
-    """
-    tool_records = api.fetch_table(table, {"id": tool_id})
-    if not tool_records:
-        return render(request, "error.html", {"error": "No tool found with that ID"})
-    if request.method == "POST":
-        form = CheckoutForm(request.POST)
-        if form.is_valid():
-            api.update_records(
-                table,
-                [
-                    {
-                        "id": tool_id,
-                        "Status": (
-                            "In Use"
-                            if request.POST["action"] == "checkout"
-                            else "Available"
-                        ),
-                        "Location": form.cleaned_data["location"],
-                    }
-                ],
-            )
-            return redirect(".")
-    else:
-        form = CheckoutForm()
-    return render(
-        request,
-        "tool.html",
-        {
-            "tool": tool_records[0],
-            "form": form,
-        },
-    )
+class ToolView(FormView):
+
+    template_name = "tool.html"
+
+    class form_class(forms.Form):
+        location = forms.CharField(max_length=200)
+
+    def get_object(self):
+        tool_records = api.fetch_table(table, {"id": self.kwargs["tool_id"]})
+        if not tool_records:
+            raise Http404("No matching tool")
+        return tool_records[0]
+
+    def form_valid(self, form) -> HttpResponse:
+        api.update_records(
+            table,
+            [
+                {
+                    "id": self.tool.id,
+                    "Status": (
+                        "In Use"
+                        if self.request.POST["action"] == "checkout"
+                        else "Available"
+                    ),
+                    "Location": form.cleaned_data["location"],
+                }
+            ],
+        )
+        return redirect(".")
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        self.tool = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tool"] = self.tool
+        return context
 
 
 @app.path("metrics/")
